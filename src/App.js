da0b051924aa8488046f6149b6a7f3d332815466
Navin <navin007.a@gmail.com>
2020-03-19 19:47:19 +0530

    Static Nav done without responsive

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Flex, Image, Link, Icon } from "@chakra-ui/core";
+import { Box, Flex, Image, Link, Icon, Button, PseudoBox } from "@chakra-ui/core";
 
 import "./App.css";
 
@@ -8,12 +8,12 @@ function App() {
     <div>
       <Box as='nav' bg='gray.800'>
         <Box maxW='7xl' mx='auto' px={{ base: 4, md: 6, lg: 8 }}>
-          <Flex align='center' justify='between' h='16'>
+          <Flex align='center' justify='space-between' h='16'>
             <Flex align='center'>
               <Box flexShrink='0'>
                 <Image h='8' w='8' src='https://tailwindui.com/img/logos/workflow-mark-on-dark.svg' />
               </Box>
-              <Box display={{ base: "none", md: "block" }}>
+              <Box d={{ base: "none", md: "block" }}>
                 <Flex ml='10' align='baseline'>
                   <Link
                     href='#test'
@@ -125,9 +125,44 @@ function App() {
                 </Flex>
               </Box>
             </Flex>
-            <Box display={{ base: "none", md: "block" }}>
+            <Box d={{ base: "none", md: "block" }}>
               <Flex align='center' ml={{ base: 4, md: 6 }}>
-                <Icon sizes='6' />
+                <PseudoBox
+                  as='button'
+                  p='1'
+                  borderWidth='2px'
+                  borderColor='transparent'
+                  color='gray.400'
+                  rounded='full'
+                  _hover={{
+                    color: "white"
+                  }}
+                  _focus={{
+                    outline: "none",
+                    color: "white",
+                    bg: "gray.700"
+                  }}
+                >
+                  <Icon size='6' name='tw-bell' />
+                </PseudoBox>
+                <PseudoBox
+                  as='button'
+                  ml='3'
+                  pos='relative'
+                  maxW='xs'
+                  align='center'
+                  fontSize='sm'
+                  rounded='full'
+                  color='white'
+                  _focus={{ outline: "none", shadow: "0 0 0 2px currentColor" }}
+                >
+                  <Image
+                    size='8'
+                    rounded='full'
+                    src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                    alt='Person literally smiling at you'
+                  />
+                </PseudoBox>
               </Flex>
             </Box>
           </Flex>
